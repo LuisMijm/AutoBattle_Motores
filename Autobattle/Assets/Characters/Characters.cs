f29cf80b2986fbbef6624d4c9259f16a74a40c10
Luis Miguel Jimenez Muñoz <jimenezmu@esat-alumni.com>
2023-09-27 23:14:21 +0100

    first organization

---
+++ Autobattle/Assets/Characters/Characters.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Characters
+{
+    public int health_;
+    public int attack_;
+    public int damage_;
+
+    
+}
